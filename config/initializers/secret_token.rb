e18af51ee3da96b44ebcb1be7bdc77264b3170d7
constantin <con@constantin-laptop.(none)>
2013-07-22 17:53:45 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vegan::Application.config.secret_token = '676c64daebd1d470d95250bfedf2716edd21f41a4d8294d2e6fc68ce12c2bd1a63a9b5c2fb4e066dbeddc334f2c40c47cb85d64f153c59252fe84ba4860652c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Vegan::Application.config.session_store :cookie_store, key: '_vegan_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Vegan::Application.config.session_store :active_record_store
